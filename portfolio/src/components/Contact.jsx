d144e74370819a52338d6b5a49fabc8616484561
DanielTruscaV1 <danieltrusca2008@gmail.com>
2025-02-19 19:58:20 +0200

    Developed a grid element for contact methods.

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -5,7 +5,24 @@ import styles from "../styles/Contact.module.css"
 const Contact = () => {
   return (
     <div className={styles.container}>
+      <p className={styles.description}>
+        If you want to reach out, you can contact me using the following methods:
+      </p>
 
+      <div className={styles.grid}>
+        <div className={styles.grid_item}>
+
+        </div>
+        <div className={styles.grid_item}>
+          
+        </div>
+        <div className={styles.grid_item}>
+          
+        </div>
+        <div className={styles.grid_item}>
+          
+        </div>
+      </div>
 
     </div>
   )
