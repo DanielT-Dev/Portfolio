023c0b214135ebfc9baef1adf8bc08953596b480
DanielTruscaV1 <danieltrusca2008@gmail.com>
2025-02-19 17:07:04 +0200

    Created the Contact component.

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
+import Contact from './components/Contact'
 import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div>
+      <h1 className="header">
+        Contact
+      </h1>
+      <Contact />
       <Footer />
     </div>
   )
---
+++ portfolio/src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import styles from "../styles/Contact.module.css"
+
+const Contact = () => {
+  return (
+    <div className={styles.container}>
+
+
+    </div>
+  )
+}
+
+export default Contact
