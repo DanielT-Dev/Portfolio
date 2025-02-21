e300899f2102ff745a6a9d2aa16b5ae6102707d5
DanielTruscaV1 <danieltrusca2008@gmail.com>
2025-02-21 19:07:33 +0200

    Created a Landing.jsx page component with a Landing.module.css stylesheet and linked them to App.jsx.

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import Navigation from './components/Navigation'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import Landing from './components/Landing'
 
 const App = () => {
   return (
     <div>
       <Navigation />
+      <Landing />
       <h1 className="header">
         Contact
       </h1>
---
+++ portfolio/src/components/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from "../styles/Landing.module.css"
+
+const Landing = () => {
+  return (
+    <div className={styles.container}>
+        Landing
+    </div>
+  )
+}
+
+export default Landing
