220ce2a6d2642db5f5e5c0624f93f24f410119b7
DanielT-Dev <danieltrusca1@outlook.com>
2025-03-05 19:48:06 +0200

    Fixed an unexpected behavior of the social buttons from the footer UI component.

---
+++ portfolio/src/posts/PostTemplate.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from "./PostTemplate.module.css"
+
+const PostTemplate = () => {
+  return (
+    <div className={styles.container}>
+        ABC
+    </div>
+  )
+}
+
+export default PostTemplate
