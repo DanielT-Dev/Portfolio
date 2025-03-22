48250cf3fc08a1b9e01564454731f60d98770761
DanielT-Dev <danieltrusca1@outlook.com>
2025-03-22 15:51:49 +0200

    Added real URL addresses for my footer social buttons.

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -4,7 +4,16 @@ import styles from "../styles/Footer.module.css"
 
 const Footer = () => {
 
-    const socials = ["github", "linkedin"]
+    const socials = [
+        {
+            name: "github",
+            url: "https://github.com/DanielT-Dev",
+        },
+        {
+            name: "linkedin",
+            url: "https://www.linkedin.com/in/marian-daniel-tru%C8%99c%C4%83-784074286/"
+        }
+    ]
 
     return (
         <div className={styles.container}>
@@ -12,11 +21,15 @@ const Footer = () => {
                 © 2025 | Marian-Daniel Trușcă
             </p>
 
-            <div className={styles.socials}>
+            <div className={styles.socials} >
             {
                 socials.map((platform, i) => (
-                    <button key={i} className={styles.social}>
-                        <a href="#" className={`fa fa-${platform}`}></a>
+                    <button 
+                        key={i} 
+                        className={styles.social}
+                        onClick={() => window.open(platform.url, '_blank')}
+                    >
+                        <i className={`fa fa-${platform.name}`} />
                     </button>
                 ))
             }
